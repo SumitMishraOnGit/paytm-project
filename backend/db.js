39b12fdab2fa523228852d1590e78fe45c82b576
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-13 18:00:55 +0530

    fix: branches structure

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,54 @@
-const mongoose = require("mongoose");
+// backend/db.js
+const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+mongoose.connect("mongodb://localhost:27017/paytm")
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
     username: {
-    type: String,
-    required: [true, "Name is required"],
-    unique:true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    trim: true,
-    minlength: [8, "Password must be at least 8 characters"],
-  },
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
-})
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
 
-const User = mongoose.model("User", UserSchema);
-module.exports = User;
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
