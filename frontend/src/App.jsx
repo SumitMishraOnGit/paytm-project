426ed6b536c605bcdb303ff0b72f1427d1990a55
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-19 04:50:04 +0530

    chore: title tweak

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './pages/signup';
 import Signin from './pages/signin';
 import Dashboard from './pages/dashboard';
-
+import LandingPage from './pages/LandingPage'; 
 
 function App() {
   return (
     <>
         <Routes>
+          <Route path="/" element={<LandingPage />} /> 
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
