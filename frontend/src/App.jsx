2f18f59935a7d94f251401a75560cd132d0faff7
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-19 04:28:07 +0530

    chore: import error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signup from './pages/signUp';
-import Signin from './pages/signIn';
+import Signup from './pages/signup';
+import Signin from './pages/signin';
 import Dashboard from './pages/dashboard';
-import SendMoney from './pages/sendMoney';
+
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} /> 
         </Routes>
     </>
   )
