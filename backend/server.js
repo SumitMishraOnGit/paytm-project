1a1170ead6d4f84fb1a2bd4cbbfdb615d143ad7f
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-20 16:55:54 +0530

    feat: added mongo connection logic

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,20 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/v1', rootRouter);
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log(" MongoDB connected");
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error(" MongoDB connection error:", err);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
