ba54085f291e0797b91919d1c90ee30e92fd18a8
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-20 18:38:47 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
 import Signup from './pages/signup';
 import Signin from './pages/signin';
 import Dashboard from './pages/dashboard';
 import LandingPage from './pages/landingpage'; 
+import SendMoney from './pages/sendmoney'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
     </>
   )
