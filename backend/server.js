41c3b661655dcadd8020f3130cc8171060d271a2
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-20 17:16:45 +0530

    fix: app.listen

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,3 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => {
   console.error(" MongoDB connection error:", err);
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
